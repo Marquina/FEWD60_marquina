02a5fc5711dc5f7ed31d31fc44f35be44e74b279
Marquina <marquinamarie@gmail.com>
2015-06-22 21:05:25 -0400

    No idea

--- Week4StartupMatchmaker/Class_lab/javascript/main.js
+++ Week4StartupMatchmaker/Class_lab/javascript/main.js
@@ -1,5 +1,28 @@
-document.write("hello world");
+//sTART1st Try Javascript Mouseover Nav 
+//document.getElementByClass("mouse-over-me").addEventListener("mouseover", function ()  {
+//  alert("You put your mouse over the div");
+//});
+//END 1st Try Javascript Mouseover Nav 
 
-document.getElementById("mouse-over-me").addEventListener("mouseover", function ()  {
-  alert("You put your mouse over the div");
-});
+//START 2nd Try Javascript Mouseover Nav 
+/*function onColor(listElement) {
+    listElement.style.background = "green";
+}
+
+function offColor(listElement) {
+    listElement.style.background = "orange"; 
+}*/
+//END 2nd Try Javascript Mouseover Nav 
+
+
+function mouseOver() {
+    document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("demo").style.color = "black";
+}
+
+function myFunction() {
+    document.getElementsByTagName("H1")[0].removeAttribute("class"); 
+}
