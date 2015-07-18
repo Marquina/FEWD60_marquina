7fd7f037d904af9071fad12b97d011c2c4823efb
Marquina <marquinamarie@gmail.com>
2015-07-18 16:13:37 -0400

    Responsive Labfor Startup Matchmaker

--- Week7/Big Lab responsive/js/main.js
+++ Week7/Big Lab responsive/js/main.js
@@ -1,8 +1,17 @@
 document.getElementsByTagName("li")[0].addEventListener("click",function(){
 	if(!this.parentNode.getAttribute("class")){
-		this.parentNode.setAttribute("class","slideDown");
-	}
+		this.parentNode.setAttribute("class","pullDown"); 
+	};
 	else{
 		this.parentNode.removeAttribute("class");
 	}
+});
+
+$(window).resize( function(){
+  console.log('working');
+ if (window.width < 768) {
+  $('a.button').text("Click Here");
+ } else {
+  $('a.button').text("Stop");
+ }
 });
